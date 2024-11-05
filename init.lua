526fcf8877e4b471c0642ad0f3d8502251e10ddf
joe-tripodi <joe@flowofcode.com>
2024-11-06 07:57:38 +0930

    add ruby_lsp

--- init.lua
+++ init.lua
@@ -626,6 +626,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        ruby_lsp = {},
 
         lua_ls = {
           -- cmd = {...},
