1dd9de356adc677501835a37460d8706151e2517
joe-tripodi <joe@flowofcode.com>
2024-11-06 08:21:18 +0930

    add custom keymaps

---
+++ lua/custom/keymaps/keymap.lua
@@ -0,0 +1,15 @@
+vim.opt.colorcolumn = '80'
+vim.opt.termguicolors = true
+
+-- Open File Explorer
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- move selected lines of code
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- when jumping by half pages you keep the cursor in the middle of the view
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
